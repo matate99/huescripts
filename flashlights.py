cfe44e851322a77b9395e80e35a8a2c719773a84
matate99 <michaelandrewtate@gmail.com>
2017-09-15 00:01:31 -0500

    Update and rename mytest.py to flashlights.py

--- flashlights.py
+++ flashlights.py
@@ -25,7 +25,7 @@ def flashlights():
     b = Bridge('192.168.65.129')
 
     light1 = getlights(1)
-    light3 = getlights(2)
+    light3 = getlights(3)
 
     # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
     #b.connect()
@@ -38,25 +38,25 @@ def flashlights():
     yellowfast = {'transitiontime' : 1, 'on' : True, 'bri' : 254, 'hue' : 11308}
     bluefast = {'transitiontime' : 1, 'on' : True, 'bri' : 254, 'hue' : 46014}
     b.set_light(1, yellowfast)
-    b.set_light(2, bluefast)
+    b.set_light(3, bluefast)
     time.sleep(1)
-    b.set_light(2, yellowfast)
+    b.set_light(3, yellowfast)
     b.set_light(1, bluefast)
     time.sleep(1)
     b.set_light(1, yellowfast)
-    b.set_light(2, bluefast)
+    b.set_light(3, bluefast)
     time.sleep(1)
-    b.set_light(2, yellowfast)
+    b.set_light(3, yellowfast)
     b.set_light(1, bluefast)
     time.sleep(1)
     b.set_light(1, yellowfast)
-    b.set_light(2, bluefast)
+    b.set_light(3, bluefast)
     time.sleep(1)
-    b.set_light(2, yellowfast)
+    b.set_light(3, yellowfast)
     b.set_light(1, bluefast)
     time.sleep(1)
     b.set_light(1, yellowfast)
-    b.set_light(2, bluefast)
+    b.set_light(3, bluefast)
 
 
     #b.set_light(1, 'on', True)
@@ -68,7 +68,7 @@ def flashlights():
     print(b.get_light(1, 'hue'))
 
     revertlights(1, light1)
-    revertlights(2, light3);
+    revertlights(3, light3);
 
 
     #r = requests.put('http://192.168.65.129/api/kl4Il3ktRM6vEN7yO0sDuwUvWKby4UHEfr4tN3d4/lights/1/state', data={"hue": 50000})
