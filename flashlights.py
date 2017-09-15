3bd2c6e5f0db828b066f346e1f592dbaa79a1dae
matate99 <michaelandrewtate@gmail.com>
2017-09-15 10:34:40 -0500

    Cleaned and added comments.

--- flashlights.py
+++ flashlights.py
@@ -8,6 +8,7 @@ from phue import Bridge
 
 
 def getlights(light):
+    #This function gets the existing light status and returns a command
     b = Bridge('192.168.65.129')
     lighton = b.get_light(light, 'on')
     lighthue = b.get_light(light, 'hue')
@@ -15,48 +16,32 @@ def getlights(light):
     revert = {'transitiontime' : 10, 'on' : lighton, 'bri' : lightbri, 'hue' : lighthue}
     return revert;
 
+
 def revertlights(light, command):
+    #This function changes the lights based on the given command
     b = Bridge('192.168.65.129')
     b.set_light(light, command);
 
-
-
 def flashlights():
+    #This function alternates the lights with the colors of the Milwaukee Brewers 
     b = Bridge('192.168.65.129')
 
     light1 = getlights(1)
     light3 = getlights(3)
 
-    # If the app is not registered and the button is not pressed, press the button and call connect() (this only needs to be run a single time)
-    #b.connect()
-
-    # Get the bridge state (This returns the full dictionary that you can explore)
-    #b.get_api()
-
-
-
     yellowfast = {'transitiontime' : 1, 'on' : True, 'bri' : 254, 'hue' : 11308}
     bluefast = {'transitiontime' : 1, 'on' : True, 'bri' : 254, 'hue' : 46014}
-    for x in range (0, 3):
+    for x in range (0, 8):
         b.set_light(1, yellowfast)
         b.set_light(3, bluefast)
         time.sleep(0.4)
         b.set_light(3, yellowfast)
         b.set_light(1, bluefast)
         time.sleep(0.4)
-    
-
-
-    print(b.get_light(3, 'hue'))
-    print(b.get_light(1, 'hue'))
 
     revertlights(1, light1)
     revertlights(3, light3);
 
-
-    #r = requests.put('http://192.168.65.129/api/kl4Il3ktRM6vEN7yO0sDuwUvWKby4UHEfr4tN3d4/lights/1/state', data={"hue": 50000})
-
-    #print(r.text)
 
 flashlights()
 
