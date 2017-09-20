0673eba6e406c6915c11ce9fc18a783ab196ec5a
matate99 <michaelandrewtate@gmail.com>
2017-09-20 14:22:32 -0500

    More Functionality

--- currenttemp.py
+++ currenttemp.py
@@ -13,14 +13,23 @@ forecast = client.getForecastWeather(q='55344')
 #print (current['current']['feelslike_f'])  # show temprature in F
 #print (forecast['forecast']['forecastday'][0]['day']['maxtemp_f'])  # show temprature in F
 
-high = forecast['forecast']['forecastday'][0]['day']['maxtemp_f'] #high in F
+highday = forecast['forecast']['forecastday'][0]['day']['maxtemp_f'] #high in F
+tempnow = current['current']['feelslike_f']
+highbroken = forecast['forecast']['forecastday'][0]['hour'][5]['maxtemp_f'] #high in F
 
-b = Bridge('192.168.65.129')
-
-lightsr = getlights(1)
+print (highbroken)
 
 #
 '''
+
+b = Bridge('192.168.65.129')
+
+lightstr = []
+
+for x in range (0, 2):
+    lightsr[x] = getlights(x + 1)
+
+
 
 high = (high**3)*.00002+(high**2)*.0035+high*.4176-5.7344
 x = (high + 10)*.0046087 + .14 #This normalized the high for the gamut conversion
@@ -35,21 +44,21 @@ b.set_light(1, color1)
 
 
 
-#
-'''
-
 for x in range (0, 12):
     high = x * 10 - 10
     print (high)
     high = (high**3)*.00002+(high**2)*.0035+high*.4176-5.7344
     x = (high + 10)*.0046087 + .14 #This normalized the high for the gamut conversion
     y = (x**3)*6.055-(x**2)*10.074+x*5.1201-.4264
-    color1 = {'transitiontime' : 1, 'on' : True, 'bri' : 100, 'xy' : [x, y]}
-    b.set_light(1, color1)
+    color = {'transitiontime' : 1, 'on' : True, 'bri' : 250, 'xy' : [x, y]}
+    for y in range (0, 2):
+        b.set_light(x + 1, color)
     time.sleep(4)
     
+for x in range (0, 2):
+    revertlights(x + 1, lightsr[x])
 
-revertlights(1, lightsr)
+'''
 
 
 
